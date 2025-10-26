5e61c1778982609980ac38caa60581ef5bac3c49
cdedood <C.J.deDood@student.tudelft.nl>
2025-10-26 11:44:03 +0100

    fixed tile placement issue

--- MovieTile.py
+++ MovieTile.py
@@ -5,8 +5,9 @@ from PIL import Image
 import os
 import sys
 
-class MediaTile(QtWidgets.QWidget, QtWidgets.QPushButton):
+class MediaTile(QtWidgets.QWidget):
     def __init__(self, media_path, width=220, parent=None):
+        super().__init__(parent)
         SQLiteDB = parent.SQLiteDB  # Assuming parent has an attribute SQLiteDB
         self.width = width
         self.height = int(width * 1.5)
